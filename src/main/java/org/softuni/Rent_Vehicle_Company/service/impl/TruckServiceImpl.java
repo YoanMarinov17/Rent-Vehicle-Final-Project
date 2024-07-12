fff343cca0d786db0726977b859da901c739d117
yoanmarinov <yoan.marinov18@gmail.com>
2024-07-13 01:10:12 +0300

    Add TruckService + Impl.

---
+++ src/main/java/org/softuni/Rent_Vehicle_Company/service/impl/TruckServiceImpl.java
@@ -0,0 +1,9 @@
+package org.softuni.Rent_Vehicle_Company.service.impl;
+
+import org.softuni.Rent_Vehicle_Company.service.TruckService;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TruckServiceImpl implements TruckService {
+}
